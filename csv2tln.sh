7e201e27f979815a796c811658ad29c75b005d52
siftgrab <siftgrab@users.noreply.github.com>
2018-05-11 12:42:24 +0400

    Update csv2tln.sh

--- csv2tln.sh
+++ csv2tln.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Converts comma separated 5 columnar TLN files with timestamps back to standard TLN Format with epoch time"
-# Timestamps must be formatted "YYYY-MM-DD HH:MM:SS" to convert to TLN
+# http://windowsir.blogspot.com/2009/02/timeline-analysis-pt-iii.html
+# Timestamps must be formatted as "YYYY-MM-DD HH:MM:SS" to process
 [ -f "$1" ] || echo "Usage: csv2tln.sh [TLN CSV File]"
 [ -f "$1" ] && cat $1  | while read d; 
 do
- timestamp=$(echo $d| awk -F',' '{print $1}'| cut -c -19)
- tlntime=$(date -d "$timestamp"  +"%s" 2>/dev/null)
+ timestamp=$(echo $d| awk -F',' '{print $1}'| grep -E '^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]')
+ [ "$timestamp" != "" ] && tlntime=$(date -d "$timestamp"  +"%s" 2>/dev/null)
  tlninfo=$(echo $d| awk -F',' '{print "|"$2"|"$3"|"$4"|"$5}')  
- echo $tlntime $tlninfo
+ [ "$timestamp" != "" ] && echo $tlntime$tlninfo
 done
