25a4b2777c2aee6e46675d826d0ec3f2cd929b28
siftgrab <siftgrab@users.noreply.github.com>
2018-05-11 12:43:59 +0400

    Update tln2csv.sh

--- tln2csv.sh
+++ tln2csv.sh
@@ -5,5 +5,6 @@
 [ -f "$1" ] || echo "Usage: tln2csv.sh [TLN File]"
 [ -f "$1" ] && cat $1  | while read d; 
   do
+    echo "$d" |grep -E '^0\|'|awk -F'|' 'NF >= 5' |awk -F'|' '{print "1970-01-01 00:00:00,"$2","$3","$4","$5}'
     echo "$d" |grep -E '^[0-9]{10}'|awk -F'|' 'NF >= 5' |awk -F'|' '{$1=strftime("%Y-%m-%d %H:%M:%S",$1)}{print $1","$2","$3","$4","$5}'
   done
