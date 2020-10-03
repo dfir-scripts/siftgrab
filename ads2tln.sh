a6ef23f8def72b63614608829228282e007e039b
jbrown <siftgrab@users.noreply.github.com>
2020-10-03 18:50:05 +0400

    Update ads2tln.sh

--- ads2tln.sh
+++ ads2tln.sh
@@ -1,32 +1,31 @@
 #!/bin/bash
+# https://medium.com/@stdout_/accessing-ntfs-extended-attributes-from-linux-f79552947981
 # https://tuxera.com/forum/viewtopic.php?f=2&t=10621
-# Extract Alternate Data Streams and Birth(crtime) timestamps from a mounted NTFS Volume 
-[ "$(sudo ls -A "$1")" ] || echo "Usage: ads2tln.sh [path to mounted NTFS Volume] [-e]"
-clear
-tempADS=$(mktemp)
-cd "$1"
-# Traverse volume and extract Alternata Data Streams and store in a temp directory as /path/file_name:ADS_name
-getfattr -Rn ntfs.streams.list . 2>/dev/null |grep -ab1 -h ntfs.streams.list=|grep -a : |sed 's/.*ntfs.streams.list\="/:/g'|while read d; 
-do 
-    printf %s "$d"|sed 's/.*# file: /\"\n"/g'|sed 's/"//g'|sed 's/^$//'>>$tempADS;
-done
+# Extracts Alternate Data Streams, MACB timestamps as a TLN from NTFS Volumes 
+export TZ='Etc/UTC'
+function ADS_extract(){
+  
+    #  scan mounted NTFS disk for Alternate Data Streams
+	getfattr -n ntfs.streams.list $ntfs_dir  >/dev/null 2>/dev/null || (echo $usage; exit)
+    getfattr -Rn ntfs.streams.list . 2>/dev/null | \
+    grep -ab1 -h ntfs.streams.list=|grep -a : |sed 's/.*ntfs.streams.list\="/:/g'| \
+    sed 's/.*# file: //'|sed 's/"$//g'|paste -d "" - -|grep -v :$ | while read ADS_file; 
+    do 
+      base_file=$(echo "$ADS_file"|sed 's/:.*//')
+      crtime=$(getfattr -h -e hex -n system.ntfs_times_be "$base_file" 2>/dev/null|grep "="|awk -F'=' '{print $2}'|grep -o '0x................')
+      epoch_time=$(echo $(($crtime/10000000-11644473600)))
+	  hr_time=$(echo $(($crtime/10000000-11644473600))| awk '{$1=strftime("%Y-%m-%d %H:%M:%S",$1)}{print $0}')
+      [ $epoch_time ] || epoch_time="0000000000"
+      #[ $hr_time !="" ] || hr_time="1970-01-01 00:00:00"
+	  [ "$output" != "csv" ] && time_stamp=$epoch_time || time_stamp=$hr_time
+	  MAC=$(stat --format=%y,%x,%z "$base_file" 2>/dev/null) 
+      [ "$ADS_file" ] && echo "$time_stamp|ADS|$comp_name||[ADS Created]: $ADS_file [MAC]: $MAC"|grep -va "ntfs.streams.list\="|tee -a $tempfile
+    done
+}
 
-#Read through file created (tempADS)
-cat $tempADS 2>/dev/null| while read d; 
-do
-    # set variable for file containing ADS
-    filepath=$(echo "$d" |sed 's/\(.*\):.*/\1/')
-    # Use the getfattr command to extract Birth time(crtime)
-    crtime=$(getfattr -h -e hex -n system.ntfs_times_be "$filepath" 2>/dev/null| grep '=' | sed -e 's/^.*=\(0x................\).*$/\1/')
-    # Convert timestamps from Windows epoch time to readable timestamp (e.g. 1970-01-01 00:00:00)
-    [ "$2" != "-e" ] && [ "$crtime" != "" ] && timestamp=$(echo $(($crtime/10000000-11644473600))| awk '{$1=strftime("%Y-%m-%d %H:%M:%S",$1)}{print $0}')
-    # Convert timestamps from Windows epoch time to Unix epoch time
-    [ "$2" == "-e" ] && [ "$crtime" != "" ] && timestamp=$(echo $(($crtime/10000000-11644473600)))
-    # Provide a null value for timestamps calculation errors 
-    [ "$2" != "-e" ] && [ "$timestamp" == "" ] && timestamp="1970-01-01 00:00:00"
-    [ "$2" == "-e" ] && [ "$timestamp" == "" ] && timestamp="0000000000"
-    # Print results to stdout based on command line 
-    [ "$2" != "-e" ] && [ "$filepath" != "" ] && echo "$timestamp,ADS,,,[ADS]: /$d"
-    [ "$2" == "-e" ] && [ "$filepath" != "" ] && echo "$timestamp|ADS|||[ADS]: /$d"
-done
-rm $tempADS
+usage="Usage: ads2tln.sh [path to mounted NTFS Volume] [-c]  (for CSV output)"
+[ "$(ls -A "$1" 2>/dev/null)" ] || $usage
+[ "$2" == "-c" ] && output="csv" 
+ntfs_dir=$1
+cd $ntfs_dir
+ADS_extract
